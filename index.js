f1e694a01c313e595ba794f561c6425578abf47b
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2026-01-20 17:19:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const a = 50;
+const b = 10;
+
+
+const r = 60
+c = a + b;
+
+console.log("The addition of a and b is = ",c)
+c = a + b;
+console.log("The addition of a and b after changing b is = ",c)
+
+{ 
+const b = 60;
+c = a + b;
+console.log("The addition of a and b inside block is = ",c)
+}
